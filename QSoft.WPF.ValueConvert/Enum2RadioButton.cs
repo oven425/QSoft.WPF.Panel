1f3f3dd65d4a4ea2d9e1bf866af16e2eaa7b516f
oven425 <oven425@yahoo.com.tw>
2024-09-24 09:00:22 +0800

    update
    1. build multitaget for treelistview
    2. test mvvm

---
+++ QSoft.WPF.ValueConvert/Enum2RadioButton.cs
@@ -0,0 +1,64 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Linq;
+
+namespace QSoft.WPF.ValueConvert
+{
+    public enum Enum2RadioButtonMatches
+    {
+        Name,
+        Value,
+        Index
+    }
+    public class Enum2RadioButton<TEnum> : IValueConverter where TEnum : struct, Enum
+    {
+        public TEnum Default { set; get; }
+        public Enum2RadioButtonMatches Match { set; get; } = Enum2RadioButtonMatches.Index;
+        readonly Dictionary<string, TEnum> m_String2Enum;
+        public Enum2RadioButton()
+        {
+            var names = Enum.GetNames<TEnum>();
+            var enums = Enum.GetValues<TEnum>();
+            this.Default = enums.FirstOrDefault();
+            var ints = Enum.GetValuesAsUnderlyingType<TEnum>().OfType<int>().Select(x => x.ToString());
+            m_String2Enum = Match switch
+            {
+                Enum2RadioButtonMatches.Index => ints.Zip(enums).ToDictionary(x => x.First, y => y.Second),
+                Enum2RadioButtonMatches.Name => names.Zip(enums).ToDictionary(x => x.First, y => y.Second),
+                Enum2RadioButtonMatches.Value => enums.Zip(enums).ToDictionary(x => x.First.ToString(), y => y.Second),
+                _ => []
+            };
+
+        }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            var enumValue = (TEnum)value;
+            var str = parameter as string;
+            if (str is null)
+            {
+                return false;
+            }
+            if (m_String2Enum.TryGetValue(str, out var result))
+            {
+                var bb = Enum.Equals(enumValue, result);
+                return bb;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var vv = this.Default;
+            if (parameter is string str)
+            {
+                vv = this.m_String2Enum[str];
+            }
+            return vv;
+        }
+    }
+
+
+
+}
+
---
+++ WpfApp_MVVM/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_MVVM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var pp = new VN1();
+            //pp.Aa = new Test();
+            var bb = pp.HasError();
+            //pp.Data.HasError();
+            //pp.HasError();
+        }
+    }
+
+
+    public partial class VN : ObservableValidatorEx
+    {
+        [ObservableProperty]
+        [Required]
+        Test aa;
+        
+    }
+
+    public partial class VN1 : ObservableValidatorEx<Test>
+    {
+        //[ObservableProperty]
+        //[Required]
+        //Test data;
+
+        //public override bool HasError()
+        //{
+        //    this.ValidateAllProperties();
+        //    //if(Data is ObservableValidatorEx ex)
+        //    //{
+        //    //    ex.HasError();
+        //    //}
+
+        //    return this.HasErrors;
+        //}
+
+        //public override bool HasError()
+        //{
+        //    var a = base.HasError();
+        //    var b = true;
+        //    if(Data is not null)
+        //    {
+        //        b = Data.HasError();
+        //    }
+        //    if (a) return true;
+        //    else if (b) return true;
+        //    return false;
+        //}
+    }
+
+    //public partial class NewVN<T> : VN<T> 
+    //{
+    //    [ObservableProperty]
+    //    [Required]
+    //    string? name;
+    //    //public override bool HasError()
+    //    //{
+    //    //    var a = base.HasError();
+    //    //    var b = true;
+    //    //    if (Data is not null)
+    //    //    {
+    //    //        b = Data.HasError();
+    //    //    }
+    //    //    if (a) return true;
+    //    //    else if (b) return true;
+    //    //    return false;
+    //    //}
+    //}
+
+    public class Test
+    {
+        [Required]
+        public string? Name { set; get; }
+
+        [Required]
+        public int Age { set; get; }
+    }
+
+    public partial class TestValidate(Test test): ObservableValidatorEx
+    {
+        [ObservableProperty]
+        [Required]
+        string? name = test.Name;
+
+        [ObservableProperty]
+        [Required]
+        int age = test.Age;
+    }
+
+    public class ObservableValidatorEx: ObservableValidator
+    {
+        virtual public bool HasError()
+        {
+            this.ValidateAllProperties();
+            return this.HasErrors;
+        }
+    }
+
+    public partial class ObservableValidatorEx<T>: ObservableValidatorEx
+    {
+        [ObservableProperty]
+        [Required]
+        T? value;
+    }
+}
