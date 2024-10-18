2058f92105ed9677dce545cda5fa3b217618e3b0
oven425 <oven425@yahoo.com.tw>
2024-10-18 11:09:44 +0800

    update
    1. study command args to ioc configuration

--- WpfApp_MVVM/App.xaml.cs
+++ WpfApp_MVVM/App.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using System.Configuration;
 using System.Data;
 using System.Diagnostics;
+using System.Runtime.InteropServices.JavaScript;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -18,13 +21,27 @@ namespace WpfApp_MVVM
         IHost? m_Host;
         public App()
         {
-            var builder = Host.CreateApplicationBuilder();
-
+            var args = Environment.GetCommandLineArgs();
+            var builder = Host.CreateApplicationBuilder(args);
+            var bb = builder.Configuration["aa"];
+            //builder.Configuration.AddCommandLine(args);
             builder.Services.AddTransient<ParameterVM_New<People>, ParameterVM_New_People>();
             builder.Services.AddTransient<ParameterVM_Modify<People>, ParameterVM_Modify_People>();
+            builder.Services.Configure<AppArgs>(config => new AppArgs()
+            {
+                aa = builder.Configuration["aa"] ?? ""
+            });
+
             m_Host = builder.Build();
             Ioc.Default.ConfigureServices(m_Host.Services);
+
+            var appargs = m_Host.Services.GetService<IOptions<AppArgs>>();
         }
+    }
+
+    public class AppArgs
+    {
+        public string aa { set; get; }
     }
 
 
