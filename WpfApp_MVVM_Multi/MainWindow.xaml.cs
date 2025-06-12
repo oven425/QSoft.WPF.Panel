d0e276f743d85e5ad95b12ac55e915d3bf964672
oven425 <oven425@yahoo.com.tw>
2025-06-12 21:33:22 +0800

    update
    1. test checkbox icommand and two user contorl ischecked

---
+++ WpfApp_MVVM_Multi/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_MVVM_Multi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainWindowVM();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is MainWindowVM vm)
+            {
+                vm.IsSet = !vm.IsSet;
+            }
+        }
+    }
+
+    public partial class MainWindowVM:ObservableObject
+    {
+        [ObservableProperty]
+        bool isSet = false;
+
+        [RelayCommand]
+        void Set(bool ss)
+        {
+
+        }
+    }
+}
