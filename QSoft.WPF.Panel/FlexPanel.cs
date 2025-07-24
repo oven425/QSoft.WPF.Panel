e8b45df2c0eb334941f406c77ad93c671646a274
oven425 <oven425@yahoo.com.tw>
2025-07-24 20:38:00 +0800

    update
    1. add border porperties by Gemini and copilot
    2. fix child and borderthickness relation position

--- QSoft.WPF.Panel/FlexPanel.cs
+++ QSoft.WPF.Panel/FlexPanel.cs
@@ -50,6 +50,30 @@ namespace QSoft.WPF.Panel
 
     public class FlexPanel : System.Windows.Controls.Panel
     {
+        public readonly static DependencyProperty BorderThicknessProperty = DependencyProperty.Register("BorderThickness", typeof(Thickness), typeof(FlexPanel), new PropertyMetadata(new Thickness()));
+        [Category("FlexPanel")]
+        public Thickness BorderThickness
+        {
+            set => this.SetValue(BorderThicknessProperty, value);
+            get => (Thickness)GetValue(BorderThicknessProperty);
+        }
+
+        public readonly static DependencyProperty CornerRadiusProperty = DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(FlexPanel), new PropertyMetadata(new CornerRadius()));
+        [Category("FlexPanel")]
+        public CornerRadius CornerRadius
+        {
+            set => this.SetValue(CornerRadiusProperty, value);
+            get => (CornerRadius)GetValue(CornerRadiusProperty);
+        }
+
+        public readonly static DependencyProperty BorderBrushProperty = DependencyProperty.Register("BorderBrush", typeof(Brush), typeof(FlexPanel), new PropertyMetadata(null));
+        [Category("FlexPanel")]
+        public Brush BorderBrush
+        {
+            set => this.SetValue(BorderBrushProperty, value);
+            get => (Brush)GetValue(BorderBrushProperty);
+        }
+
         public readonly static DependencyProperty JustifyContentProperty = DependencyProperty.Register("JustifyContent", typeof(JustifyContent), typeof(FlexPanel), new FrameworkPropertyMetadata(JustifyContent.SpaceBetween, FrameworkPropertyMetadataOptions.AffectsMeasure));
         [Category("FlexPanel")]
         public JustifyContent JustifyContent
@@ -96,6 +120,158 @@ namespace QSoft.WPF.Panel
         public static AlignSelf GetAlignSelf(DependencyObject obj) => (AlignSelf)obj.GetValue(AlignSelfProperty);
         public static void SetAlignSelf(DependencyObject obj, AlignSelf value) => obj.SetValue(AlignSelfProperty, value);
 
+
+        protected override void OnRender(DrawingContext dc)
+        {
+            //base.OnRender(dc);
+            // 取得目前的邊框厚度、圓角和渲染尺寸
+            Thickness thickness = this.BorderThickness;
+            CornerRadius cornerRadius = this.CornerRadius;
+
+            // RenderSize 是控制項最終被分配到的繪圖區域大小
+            Rect renderRect = new Rect(new Point(0, 0), this.RenderSize);
+
+            // 1. 繪製背景
+            if (this.Background != null)
+            {
+                // 使用 DrawRoundedRectangle 繪製背景，Pen 設為 null 表示不繪製邊框
+                //dc.DrawRoundedRectangle(this.Background, null, renderRect, cornerRadius);
+            }
+
+            // 2. 繪製邊框
+            // 檢查是否有邊框畫刷，且邊框厚度不為零
+            //if (this.BorderBrush != null && !thickness.Equals(new Thickness()))
+            {
+                // 使用最穩健的方法：建立一個代表邊框的 Geometry 並繪製它
+
+                // 定義外部矩形的邊界
+                Rect outerRect = renderRect;
+
+                // 根據邊框厚度計算內部矩形的邊界
+                // 內部矩形向內縮排
+                Rect innerRect = new Rect(
+                    outerRect.Left + thickness.Left,
+                    outerRect.Top + thickness.Top,
+                    Math.Max(0.0, outerRect.Width - thickness.Left - thickness.Right),
+                    Math.Max(0.0, outerRect.Height - thickness.Top - thickness.Bottom)
+                );
+
+                // 計算內部圓角半徑
+                // 內部圓角需要根據外部圓角和邊框厚度進行縮減
+                CornerRadius innerCornerRadius = new CornerRadius(
+                    Math.Max(0.0, cornerRadius.TopLeft - Math.Max(thickness.Left, thickness.Top) / 2.0),
+                    Math.Max(0.0, cornerRadius.TopRight - Math.Max(thickness.Right, thickness.Top) / 2.0),
+                    Math.Max(0.0, cornerRadius.BottomRight - Math.Max(thickness.Right, thickness.Bottom) / 2.0),
+                    Math.Max(0.0, cornerRadius.BottomLeft - Math.Max(thickness.Left, thickness.Bottom) / 2.0)
+                );
+
+                // 建立外部和內部幾何圖形
+                // RoundedRectangleGeometry 是描述圓角矩形的 Geometry
+                var outerGeometry = CreateRoundedRectangleGeometry(outerRect, cornerRadius);
+                var innerGeometry = CreateRoundedRectangleGeometry(innerRect, innerCornerRadius);
+
+                // 使用 CombinedGeometry 將兩個幾何圖形組合起來
+                // GeometryCombineMode.Exclude 會從第一個圖形中減去第二個圖形
+                // 這樣就得到了中空的邊框形狀
+                var borderGeometry = new CombinedGeometry(
+                    GeometryCombineMode.Exclude,
+                    outerGeometry,
+                    innerGeometry
+                );
+
+                // 最後，使用 DrawGeometry 繪製這個計算出來的邊框形狀
+                // Pen 設為 null，因為我們是直接填充(Fill)這個 Geometry
+                dc.DrawGeometry(this.BorderBrush, null, borderGeometry);
+                dc.DrawGeometry(this.Background, null, innerGeometry);
+            }
+        }
+
+        PathGeometry CreateRoundedRectangleGeometry(Rect rect, CornerRadius cornerRadius)
+        {
+            // 建立 PathGeometry 和 PathFigure
+            var geometry = new PathGeometry();
+            var figure = new PathFigure();
+            geometry.Figures.Add(figure);
+
+            // 校正 CornerRadius，確保半徑不會大於矩形尺寸的一半
+            double left = rect.Left;
+            double top = rect.Top;
+            double right = rect.Right;
+            double bottom = rect.Bottom;
+
+            // 左上角 (Top-Left)
+            double radiusX_TL = Math.Min(cornerRadius.TopLeft, (right - left) / 2.0);
+            double radiusY_TL = Math.Min(cornerRadius.TopLeft, (bottom - top) / 2.0);
+
+            // 右上角 (Top-Right)
+            double radiusX_TR = Math.Min(cornerRadius.TopRight, (right - left) / 2.0);
+            double radiusY_TR = Math.Min(cornerRadius.TopRight, (bottom - top) / 2.0);
+
+            // 右下角 (Bottom-Right)
+            double radiusX_BR = Math.Min(cornerRadius.BottomRight, (right - left) / 2.0);
+            double radiusY_BR = Math.Min(cornerRadius.BottomRight, (bottom - top) / 2.0);
+
+            // 左下角 (Bottom-Left)
+            double radiusX_BL = Math.Min(cornerRadius.BottomLeft, (right - left) / 2.0);
+            double radiusY_BL = Math.Min(cornerRadius.BottomLeft, (bottom - top) / 2.0);
+
+            // 設定 PathFigure 的起始點 (上邊緣，左上圓角之後)
+            figure.StartPoint = new Point(left + radiusX_TL, top);
+
+            // 1. 上邊緣 (Top Edge)
+            figure.Segments.Add(new LineSegment(new Point(right - radiusX_TR, top), true));
+
+            // 2. 右上角 (Top-Right Corner)
+            figure.Segments.Add(new ArcSegment(
+                new Point(right, top + radiusY_TR),
+                new Size(radiusX_TR, radiusY_TR),
+                90, // 旋轉角度
+                false, // isLargeArc
+                SweepDirection.Clockwise,
+                true));
+
+            // 3. 右邊緣 (Right Edge)
+            figure.Segments.Add(new LineSegment(new Point(right, bottom - radiusY_BR), true));
+
+            // 4. 右下角 (Bottom-Right Corner)
+            figure.Segments.Add(new ArcSegment(
+                new Point(right - radiusX_BR, bottom),
+                new Size(radiusX_BR, radiusY_BR),
+                90,
+                false,
+                SweepDirection.Clockwise,
+                true));
+
+            // 5. 下邊緣 (Bottom Edge)
+            figure.Segments.Add(new LineSegment(new Point(left + radiusX_BL, bottom), true));
+
+            // 6. 左下角 (Bottom-Left Corner)
+            figure.Segments.Add(new ArcSegment(
+                new Point(left, bottom - radiusY_BL),
+                new Size(radiusX_BL, radiusY_BL),
+                90,
+                false,
+                SweepDirection.Clockwise,
+                true));
+
+            // 7. 左邊緣 (Left Edge)
+            figure.Segments.Add(new LineSegment(new Point(left, top + radiusY_TL), true));
+
+            // 8. 左上角 (Top-Left Corner)
+            figure.Segments.Add(new ArcSegment(
+                figure.StartPoint, // 回到起點
+                new Size(radiusX_TL, radiusY_TL),
+                90,
+                false,
+                SweepDirection.Clockwise,
+                true));
+
+            // 關閉圖形
+            figure.IsClosed = true;
+
+            return geometry;
+        }
+
         protected override Size MeasureOverride(Size availableSize)
         {
             if (InternalChildren.Count == 0)
@@ -119,8 +295,8 @@ namespace QSoft.WPF.Panel
                     sz.Height = ll.Sum(x => x.DesiredSize.Height) + totalgap;
                     break;
             }
-            sz.Width = sz.Width + this.Padding.Left + this.Padding.Right;
-            sz.Height = sz.Height + this.Padding.Top + this.Padding.Bottom;
+            sz.Width = sz.Width + this.Padding.Left + this.Padding.Right+this.BorderThickness.Left+this.BorderThickness.Right;
+            sz.Height = sz.Height + this.Padding.Top + this.Padding.Bottom+this.BorderThickness.Top+this.BorderThickness.Bottom;
             if (sz.Width > availableSize.Width)
             {
                 sz.Width = availableSize.Width;
@@ -176,7 +352,7 @@ namespace QSoft.WPF.Panel
                             switch (this.FlexDirection)
                             {
                                 case FlexDirection.Row:
-                                    rc.Y = this.Padding.Top;
+                                    rc.Y = this.Padding.Top+this.BorderThickness.Top;
                                     rc.Height = oo.Key.DesiredSize.Height;
                                     break;
                                 case FlexDirection.Column:
@@ -191,7 +367,7 @@ namespace QSoft.WPF.Panel
                             switch (this.FlexDirection)
                             {
                                 case FlexDirection.Row:
-                                    rc.Y = finalSize.Height - oo.Key.DesiredSize.Height - this.Padding.Bottom;
+                                    rc.Y = finalSize.Height - oo.Key.DesiredSize.Height - this.Padding.Bottom - this.BorderThickness.Bottom;
                                     rc.Height = oo.Key.DesiredSize.Height;
                                     break;
                                 case FlexDirection.Column:
@@ -251,8 +427,8 @@ namespace QSoft.WPF.Panel
         {
             var item_w = 0.0;
             var item_h = 0.0;
-            double x = this.Padding.Left;
-            double y = this.Padding.Top;
+            double x = this.Padding.Left+this.BorderThickness.Left;
+            double y = this.Padding.Top+this.BorderThickness.Top;
 
             switch (this.JustifyContent)
             {
@@ -293,7 +469,7 @@ namespace QSoft.WPF.Panel
                     switch (this.FlexDirection)
                     {
                         case FlexDirection.Row:
-                            x = finalSize.Width - this.Padding.Right;
+                            x = finalSize.Width - this.Padding.Right - this.BorderThickness.Right;
                             for (int i = els.Count - 1; i >= 0; i--)
                             {
                                 var child = els.ElementAt(i).Key;
