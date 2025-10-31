3244f728e7147e68bcbdd4ba5738406ab7d81ee9
oven425 <oven425@yahoo.com.tw>
2025-10-31 08:33:30 +0800

    update
    1. fix test gap auto number only
    2. recreate flex test ui at frontend css
    3. study tailwindcss dynamic class name

--- my-flex-app/src/App.tsx
+++ my-flex-app/src/App.tsx
@@ -1,33 +1,53 @@
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [gap, setGap] = useState(2.2);
+  const [direction, setDirection] = useState('flex-row');
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='w-screen h-screen bg-red-300 flex flex-col'>
+        <div className='border-b-2'>
+          <h1>Flex panel</h1>
+        </div>   
+        <div className='flex flex-row grow bg-yellow-200 '>
+          <div className='bg-green-500 w-36 flex justify-start flex-col border-r-2'>
+            {/* <Button>Add item</Button> */}
+            <h5>Direction</h5>
+            <select value={direction} onChange={e=>setDirection(e.target.value)}>
+              <option value="flex-row">Row</option>
+              <option value="flex-col">Column</option>
+            </select>
+            <h5>Justify-content</h5>
+            <select>
+              <option value="justify-start">Start</option>
+              <option>Center</option>
+              <option>End</option>
+            </select>
+            <h5>Align-items</h5>
+            <select>
+              <option>Start</option>
+              <option>Center</option>
+              <option>End</option>
+            </select>
+            <h5>Gap</h5>
+            {/* <input value={gap} onChange={x=>setGap(x.target.value)}/> */}
+            <button onClick={()=>setGap(x=>x+2)}>AAA</button>
+            <h5>Padding</h5>
+            <input/>
+          </div>
+          <div className={`bg-blue-500 flex ${direction} align-top justify-start gap-[${gap}px] grow`}>
+          {/* <div className='bg-blue-500 flex align-top justify-start gap-[2px] grow'> */}
+            <div className='bg-pink-500 w-5 h-5'>
+
+            </div>
+            <div className='bg-purple-500 w-5 h-5'>
+
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ my-flex-app/tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  safelist: [
+    {
+        pattern: /gap-[(.*)px]/,
+    }
+  ]
+}
