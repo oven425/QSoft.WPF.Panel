e9bea04ed45fd01e974468544bd6fce7f67ff792
oven425 <oven425@yahoo.com.tw>
2025-11-03 08:21:25 +0800

    update
    1. add flex css binding
    2. test basis property by css

--- my-flex-app/src/App.tsx
+++ my-flex-app/src/App.tsx
@@ -1,10 +1,12 @@
 
 import { useState } from 'react'
 import './App.css'
-
+import deleteLogo from './delete-alert-outline.svg'
 function App() {
-  const [gap, setGap] = useState(2.2);
+  const [gap, setGap] = useState("0");
   const [direction, setDirection] = useState('flex-row');
+  const [justifyContent, setJustifyContent] = useState('justify-start');
+  const [alignItems, setAlignItems] = useState('items-start');
   return (
     <>
       <div className='w-screen h-screen bg-red-300 flex flex-col'>
@@ -20,30 +22,36 @@ function App() {
               <option value="flex-col">Column</option>
             </select>
             <h5>Justify-content</h5>
-            <select>
+            <select value={justifyContent} onChange={e=>setJustifyContent(e.target.value)}>
               <option value="justify-start">Start</option>
-              <option>Center</option>
-              <option>End</option>
+              <option value="justify-center">Center</option>
+              <option value="justify-end">End</option>
+              <option value="justify-between">Between</option>
+              <option value="justify-around">Around</option>
+              <option value="justify-evenly">Evenly</option>
             </select>
             <h5>Align-items</h5>
-            <select>
-              <option>Start</option>
-              <option>Center</option>
-              <option>End</option>
+            <select value={alignItems} onChange={e=>setAlignItems(e.target.value)}>
+              <option value="items-start">Start</option>
+              <option value="items-center">Center</option>
+              <option value="items-end">End</option>
+              <option value="items-stretch">Stretch</option>
             </select>
             <h5>Gap</h5>
-            {/* <input value={gap} onChange={x=>setGap(x.target.value)}/> */}
-            <button onClick={()=>setGap(x=>x+2)}>AAA</button>
+            <input value={gap} onChange={x=>setGap(x.target.value)} type='number'/>
+            {/* <button onClick={()=>setGap(x=>x+2)}>AAA</button> */}
             <h5>Padding</h5>
             <input/>
           </div>
-          <div className={`bg-blue-500 flex ${direction} align-top justify-start gap-[${gap}px] grow`}>
-          {/* <div className='bg-blue-500 flex align-top justify-start gap-[2px] grow'> */}
-            <div className='bg-pink-500 w-5 h-5'>
-
+          <div style={{gap:`${gap}px`}} className={`bg-blue-500 flex ${direction} ${alignItems} ${justifyContent}  grow`}>
+            <div className='bg-pink-500'>
+              AAA
             </div>
-            <div className='bg-purple-500 w-5 h-5'>
-
+            <div className='bg-purple-500'>
+              BBB
+            </div>
+            <div className='bg-yellow-400 border-amber-900 border-2 basis-10 flex hover:bg-red-500 hover:border-gray-700'>
+              <img src={deleteLogo} className="logo react" alt="React logo" />
             </div>
           </div>
         </div>
