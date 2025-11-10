cbb9b3419e6b707252fcd63cfc6eed1e67b17681
oven425 <oven425@yahoo.com.tw>
2025-11-10 08:25:36 +0800

    update
    1. test disable translate for chrome
    2. fix the basics property not work, but should be able to do better.

--- my-flex-app/src/App.tsx
+++ my-flex-app/src/App.tsx
@@ -8,20 +8,22 @@ function App() {
   const [justifyContent, setJustifyContent] = useState('justify-start');
   const [alignItems, setAlignItems] = useState('items-start');
   const [items, setItems] = useState<Itemd[]>([]); 
-  const [item, setItem] = useState<Itemd>({id: 0, alignSelf: 'self-start',grow:0 , basis:"auto"});
+  const [item, setItem] = useState<Itemd>({id: 0, alignSelf: 'self-start',grow:0,shrink:0 , basis:"auto"});
   const id = useRef(0);
-  type flexBasis = number | "auto";
+  type FlexBasis = "auto" | `${number}px` | `${number}%`;
   type Itemd = {
       id:number
       alignSelf:string
       grow:number
-      basis:flexBasis
+      shrink:number
+      basis:FlexBasis
   };
   const addItem = ()=>{
     const admin: Itemd = {
       id : id.current++,
       alignSelf: 'self-center',
       grow:0,
+      shrink:0,
       basis:"auto"
     };
     setItems(prevItems => [...prevItems, admin]); 
@@ -45,7 +47,15 @@ function App() {
     }));
   }
 
-  const basisChange = (newvalue:flexBasis)=>{
+  const shrinkChange = (newvalue:number)=>{
+    setItem(x=>({
+      ... x,
+      shrink: newvalue
+    }));
+  }
+
+  const basisChange = (newvalue:FlexBasis)=>{
+    console.log(`basis:${newvalue}`)
     setItem(x=>({
       ... x,
       basis: newvalue
@@ -61,7 +71,7 @@ function App() {
             return x;
           });
         });
-  },[item]);
+      },[item]);
 
   return (
     <>
@@ -72,6 +82,7 @@ function App() {
         <div className='flex flex-row grow bg-yellow-200 '>
           <div className='bg-green-500 w-36 flex justify-start flex-col border-r-2'>
             <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded' onClick={()=>addItem()}>add item</button>
+            <h4>Container</h4>
             <h5>Direction</h5>
             <select value={direction} onChange={e=>setDirection(e.target.value)}>
               <option value="flex-row">Row</option>
@@ -107,20 +118,25 @@ function App() {
             </select>
             <h5>Flex-grow</h5>
             <input value={item.grow} onChange={x=>growChange(Number(x.target.value))} type='number'/>
+            <h5>Flex-shrink</h5>
+            <input value={item.shrink} onChange={x=>shrinkChange(Number(x.target.value))} type='number'/>
             <h5>Flex-basis</h5>
-            <input placeholder="auto" value={typeof item.basis === "number" ? item.basis : ""} onChange={x=>basisChange(x.target.value === "" ? "auto" : Number(x.target.value))} type='number'/>
+            <input placeholder='auto' value={item.basis} onChange={x=>basisChange(x.target.value as FlexBasis)}/>
           </div>
           <div style={{gap:`${gap}px`}} className={`bg-blue-500 flex ${direction} ${alignItems} ${justifyContent}  grow`}>
             {
               items.map((x,i)=>(
-                <div key={i} style={{flexGrow:`${x.grow}`, flexBasis:`${x.basis}`}} className={`bg-amber-400 flex ${x.alignSelf}`} >
-                  <div onClick={()=>setItem(x)} className='self-center'>Index:{i} id:{x.id}</div>
-                  <div onClick={()=>removeItem(x)} className='bg-yellow-400 border-amber-900 w-10 hover:bg-red-500 hover:border-gray-700'>
+                <div key={i} style={{flexGrow:`${x.grow}`, flexShrink:`${x.shrink}`, flexBasis:`${x.basis}`}} className={`bg-amber-400 flex ${x.alignSelf} p-0.5`} >
+                  <div onClick={()=>setItem(x)} className='self-center p-1 grow'>Index:{i}</div>
+                  <div onClick={()=>removeItem(x)} className='flex items-center bg-yellow-400 border-amber-900 w-10 hover:bg-red-500 hover:border-gray-700'>
                     <img src={deleteLogo} className="logo react" alt="React logo" />
                   </div>
                 </div>
               ))
             }
+            {/* <div style={{flexBasis:20, flexGrow:0, flexShrink:0}} className=' bg-red-400'>A</div>
+            <div className='basis-10 bg-green-400'>B</div>
+            <div className='basis-15 bg-blue-400'>C</div> */}
           </div>
         </div>
       </div>
