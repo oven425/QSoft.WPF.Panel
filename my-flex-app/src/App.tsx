b0df86aac1740abd6044319a3e9213979dee4dba
oven425 <oven425@yahoo.com.tw>
2026-01-07 08:41:30 +0800

    update
    1. study text in small div effect
    2. study wpf textblock in small border

--- my-flex-app/src/App.tsx
+++ my-flex-app/src/App.tsx
@@ -139,8 +139,8 @@ function App() {
                 ))
               }
 
-              <div className='bg-gray-400 min-w-0 overflow-hidden flex p-0.5 border rounded-sm dark:border-gray-600'>
-                <div className='self-center px-3 py-1 grow'>1234567890</div>
+              <div className='bg-gray-400 min-w-0 w-16 overflow-hidden flex p-0.5 border rounded-sm dark:border-gray-600'>
+                <p className='self-center px-3 py-1 overflow-hidden '>1234567890</p>
                 <div className="border-r dark:border-gray-600 border-neutral-400"></div>
                 <div className={`flex items-center  fill-gray-950 w-10 dark:hover:bg-gray-600`}>
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" /></svg>
@@ -150,10 +150,10 @@ function App() {
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M4 19V7H16V19C16 20.1 15.1 21 14 21H6C4.9 21 4 20.1 4 19M6 9V19H14V9H6M13.5 4H17V6H3V4H6.5L7.5 3H12.5L13.5 4M19 17V15H21V17H19M19 13V7H21V13H19Z" /></svg>
                 </div>
               </div>
-              <div className='bg-gray-400  flex p-0.5 border rounded-sm dark:border-gray-600'>
-                <p style={{overflow:'hidden', minWidth:0, textOverflow:'ellipsis'}}>1234567890</p>
-              </div>
-              <div className='bg-gray-400 flex p-0.5 border rounded-sm dark:border-gray-600'>
+              {/* <div className='bg-gray-400 w-12 overflow-hidden p-0.5 border rounded-sm dark:border-gray-600'>
+                <div className='truncate'>1234567890</div>
+              </div> */}
+              {/* <div className='bg-gray-400 flex p-0.5 border rounded-sm dark:border-gray-600'>
                 <p>1234 567890</p>
               </div>
               <div className='bg-gray-400 flex p-0.5 border rounded-sm dark:border-gray-600'>
@@ -162,12 +162,9 @@ function App() {
               <div className='bg-gray-400 flex p-0.5 border rounded-sm dark:border-gray-600'>
                 <p>1234567890</p>
               </div>
-              <div className='bg-gray-400 flex p-0.5 border rounded-sm dark:border-gray-600'>
+<div className='bg-gray-400 flex p-0.5 border rounded-sm dark:border-gray-600'>
                 <p>1234567890</p>
-              </div>
-              <div className='bg-gray-400 min-w-0 overflow-hidden flex p-0.5 border rounded-sm dark:border-gray-600'>
-                <p>1234567890</p>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
